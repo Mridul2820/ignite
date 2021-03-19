e39d458a1d49c62c2c552f6a2aa62acf6fa62e5a
Mridul2820 <immridulpanda2820@gmail.com>
2021-03-19 19:55:47 +0530

    creating the reducers

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Redux Setup
+import { createStore } from "redux";
+import rootReducer from './reducers/index'
+import provider, { Provider } from 'react-redux'
+
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
