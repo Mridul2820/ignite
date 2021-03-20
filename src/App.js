12c22addf56d7e78ba090c0f30f36857f4b19a7f
Mridul2820 <immridulpanda2820@gmail.com>
2021-03-20 11:56:51 +0530

    showing games in dom

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Home from './pages/Home'
 const App = () => {
     return (
         <div>
-            <h1>Ignite</h1>
             <Home />
         </div>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,57 @@
 import { useEffect } from 'react'
 
 // Redux
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gameAction";
 
+// Components
+import Game from '../components/Game'
+
+// Styling & Animation
+import styled from 'styled-components'
+import { motion } from "framer-motion";
+
 const Home = () => {
+    // Setch Games
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(loadGames())
-    }, [])
+    }, [dispatch])
+
+    // Get the data back
+    const { popular, newGames, upcoming } = useSelector((state => state.games))
 
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <StyledGameList>
+            <h2>Upcoming Games</h2>
+            <StyledGames>
+                {upcoming.map(game => (
+                    <Game 
+                        key={game.id}
+                        name={game.name} 
+                        released={game.released}
+                        id={game.id}
+                        image={game.background_image}
+                    />
+                ))}
+            </StyledGames>
+        </StyledGameList>
     )
 }
+
+const StyledGameList = styled(motion.div)`
+    padding: 0rem 5rem;
+
+    h2 {
+        padding: 2rem 0rem;
+    }
+`
+
+const StyledGames = styled(motion.div)`
+    min-height: 80vh;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+    gap: 3rem 2rem;
+`
 
 export default Home
